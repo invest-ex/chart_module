c32a6ee95ed89b76e302008fdf335f9dc2f3e22d
carl12 <carl17r@gmail.com>
2019-04-22 15:49:20 -0700

    Update cassandra read

--- readToCassandra.js
+++ readToCassandra.js
@@ -3,7 +3,9 @@ const readLine = require('readline');
 const { Writable } = require('stream');
 const cassandra = require('cassandra-driver');
 
-const readStream = fs.createReadStream('./files/dataSmall.txt');
+let streams = 32;
+console.time(`Cassandra write ${streams} streams`);
+const readStream = fs.createReadStream('./files/data.txt');
 
 const lineReader = readLine.createInterface({
 	input: readStream,
@@ -12,7 +14,7 @@ const lineReader = readLine.createInterface({
 
 const client = new cassandra.Client({ 
   contactPoints: ['localhost'], 
-  keyspace: 'investex',
+  keyspace: 'cycling',
   localDataCenter: 'datacenter1' 
 });
 
@@ -22,24 +24,42 @@ class DbWriteManager {
 		this.restartFunc = restartFunc;
 		this.numStreams = numStreams;
 		this.streams = [];
+    this.streamsFull = [];
 		let stream;
 		for (let i = 0; i < numStreams; i++) {
-			stream = new Writable({write: writeFunc});
-			stream.on('drain', restartFunc);
+			stream = new Writable({write: function writeFunc(data, encoding, cb) {
+        client.execute(createQuery(data), cb);
+      }});
+      this.streamsFull.push(false);
+			stream.on('drain', ()=> {
+        this.streamsFull[i] = false;
+        restartFunc();
+      });
 			this.streams.push(stream);
 		}
 	}
 
 	write(data) {
-		for (let i = 0
-			; i < this.numStreams 
-			&& !this.streams[i].write(data)
-			; i++)
-		return i < this.numStreams;
+    let j = 0;
+    while (j < this.numStreams) {
+      if (this.streamsFull[j]) {
+        j ++; 
+        continue;
+      }
+      let worked = this.streams[j].write(data);
+      if (!worked)
+      {
+        this.streamsFull[j] = true;
+      } 
+      break;
+
+    }
+		return j < this.numStreams;
 	}
 }
-
+let q = 0;
 let createQuery = (buffer) => {
+  q++;
 	let obj = JSON.parse(buffer.toString());
 	let {stockInfo, stockData} = obj;
 	query = `
@@ -75,12 +95,13 @@ let createQuery = (buffer) => {
 	`;
 	return query;
 }
-
+let q2 = 0;
 let writeFunc = (data, encoding, cb) => {
 	client.execute(createQuery(data), cb);
 }
 
 let restartLineReader = () => {
+
 	while (bufferLines.length) {
     let line = bufferLines.pop();
     if (!dbWrite.write(line)) {
@@ -91,14 +112,24 @@ let restartLineReader = () => {
   lineReader.resume();
 
 }
-
-const dbWrite = new DbWriteManager(writeFunc, restartLineReader, 1);
+const dbWrite = new DbWriteManager(writeFunc, restartLineReader, streams);
 
 let bufferLines = [];
-
+let lineN = 0;
+let lastLineN = -1;
 lineReader.on('line', function (line) {
+  lineN++;
 	if(!dbWrite.write(line)) {
 		lineReader.pause();
 		bufferLines.push(line);
 	}
 });
+let tick = 0;
+setInterval(()=>{
+  console.log(lineN, '/', ++tick);
+  if (lineN === lastLineN) {
+    console.timeEnd(`Cassandra write ${streams} streams`);
+    process.exit();
+  } 
+  lastLineN = lineN;
+}, 1000);
