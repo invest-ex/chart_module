420bc7f952d0ee41af7859a1ed2f435e7df94da4
carl12 <carl17r@gmail.com>
2019-04-22 22:11:29 -0700

    Implement parameterized db stream manager

---
+++ cassandraSeed.js
@@ -0,0 +1,54 @@
+const cassandra = require('cassandra-driver');
+const FileToDBManager = require('./fileToDB.js');
+
+let createQuery = (buffer) => {
+	let obj = JSON.parse(buffer.toString());
+	let {stockInfo, stockData} = obj;
+	query = `
+		INSERT INTO stocks (
+			StockId, 
+        	AverageStock, 
+        	ChangePercent,
+        	RelatedTags,
+        	CompanyName,
+        	NoOfOwners,
+        	RecommendationPercent,
+        	day,
+        	week,
+        	month,
+        	threeMonth,
+        	year,
+        	fiveYear
+        ) VALUES (
+        	'${obj.stockId}', 
+        	${obj.averageStock}, 
+        	${obj.changePercent},
+        	[${stockInfo.relatedTags}],
+        	'${stockInfo.stockCompany}',
+        	${stockInfo.noOfOwners},
+        	${stockInfo.recommendationPercent},
+        	[${stockData.day}],
+        	[${stockData.week}],
+        	[${stockData.month}],
+        	[${stockData.threeMonth}],
+        	[${stockData.year}],
+        	[${stockData.fiveYear}]
+		);
+	`;
+	return query;
+}
+
+
+
+const client = new cassandra.Client({ 
+  contactPoints: ['localhost'], 
+  keyspace: 'cycling',
+  localDataCenter: 'datacenter1' 
+});
+
+let writeFunc = (data, encoding, cb) => {
+	client.execute(createQuery(data), cb);
+}
+
+var a = new FileToDBManager(writeFunc, './files/dataSmall.txt', 32);
+
---
+++ fileToDB.js
@@ -0,0 +1,102 @@
+const fs = require('fs');
+const readLine = require('readline');
+const { Writable } = require('stream');
+const cassandra = require('cassandra-driver');
+
+let streams = 32;
+
+const readStream = fs.createReadStream('./files/dataSmall.txt');
+
+
+
+class DbWriteManager {
+	constructor(writeFunc, restartFunc, numStreams = 4) {
+		this.writeFunc = writeFunc;
+		this.restartFunc = restartFunc;
+		this.numStreams = numStreams;
+		this.streams = [];
+    this.streamsFull = [];
+		let stream;
+		for (let i = 0; i < numStreams; i++) {
+			stream = new Writable({write: writeFunc});
+      this.streamsFull.push(false);
+			stream.on('drain', ()=> {
+        this.streamsFull[i] = false;
+        restartFunc();
+      });
+			this.streams.push(stream);
+		}
+	}
+
+	write(data) {
+    let j = 0;
+    while (j < this.numStreams) {
+      if (this.streamsFull[j]) {
+        j ++; 
+        continue;
+      }
+      let worked = this.streams[j].write(data);
+      if (!worked)
+      {
+        this.streamsFull[j] = true;
+      } 
+      break;
+
+    }
+		return j < this.numStreams;
+	}
+}
+
+class FileToDBManager {
+  constructor(writeFunc, filePath, streams = 32) {
+
+    this.restartLineReader = this.restartLineReader.bind(this);
+    this.onReaderLine = this.onReaderLine.bind(this);
+    this.interValPrint = this.interValPrint.bind(this);
+    
+    this.streams = streams;
+    this.dbWrite = new DbWriteManager(writeFunc, this.restartLineReader, streams);
+    this.lineReader = readLine.createInterface({
+      input: fs.createReadStream(filePath),
+      console: false
+    });
+    this.lineReader.on('line', this.onReaderLine);
+    this.bufferLines = [];
+    this.numLines = 0;
+    this.tick = 0;
+    console.time(`Cassandra write ${this.streams} streams`);
+    setInterval(this.interValPrint, 1000);
+
+  }
+
+  restartLineReader() {
+    while (this.bufferLines.length) {
+      let line = this.bufferLines.pop();
+      if (!this.dbWrite.write(line)) {
+        this.bufferLines.push(line);
+        return;
+      }
+    }
+    this.lineReader.resume();
+  }
+
+  onReaderLine(line) {
+    this.numLines ++;
+    if(!this.dbWrite.write(line)) {
+      this.lineReader.pause();
+      this.bufferLines.push(line);
+    }
+  }
+
+  interValPrint() {
+    console.log(this.numLines, '/', ++this.tick);
+    if (this.lastNumLines === this.numLines) {
+      console.timeEnd(`Cassandra write ${this.streams} streams`);
+      process.exit();
+    } 
+    this.lastNumLines = this.numLines;
+  }
+}
+
+
+module.exports = FileToDBManager;
