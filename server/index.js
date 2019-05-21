80f1623713d3ac4ba915f2eaddd8f39f265606dd
EC2 Default User <ec2-user@ip-172-31-25-71.us-east-2.compute.internal>
2019-05-21 18:17:42 +0000

    Change html to be served from variable

--- server/index.js
+++ server/index.js
@@ -11,6 +11,11 @@ const port = 80;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.send(html);
+});
+
 app.use(express.static(path.join(__dirname, '/../public/dist')));
 
 app.listen(port, () => {
@@ -46,7 +51,26 @@ app.get('/api/chart/:stockId', (req, res) => {
   // }) 
 
 })
+const html = `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Stock Chart</title>
+  <link rel="stylesheet" href="http://github.hubspot.com/odometer/themes/odometer-theme-minimal.css" />
+  <script src="http://github.hubspot.com/odometer/odometer.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <div id="stock-chart"></div>
+  <script src="/bundle.js"></script>
+</body>
+</html>
+`;
 
 app.get('/stocks/:stockId', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../public/dist/index.html'));
-})
+  res.send(html);
+  //res.sendFile(path.join(__dirname, '/../public/dist/index.html'));
+});
